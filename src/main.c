29f303b16c038d67beb129e460ded405802ea7cf
Theodore Karatapanis <Theodoros.Karatapanis@rt-rk.com>
2020-01-20 16:07:23 +0100

    Conformed main() to use the arg parser

--- src/main.c
+++ src/main.c
@@ -13,8 +13,9 @@
 #define NOT_APPLICABLE          0
 
 #define ARG0    {"--no-white-spaces","-w", "Automatically skip all the white spaces", argBinary}
+#define ARG1    {"--test-length", "-t", "Set the test length for the automatic tests", argVariable, "71"}
 
-static argument arguments[] = {ARG0};
+static argument arguments[] = {ARG0, ARG1};
 
 int main(int argc, char** argv)
 {
@@ -23,42 +24,13 @@ int main(int argc, char** argv)
     registerArguments("typing test",arguments, sizeof(arguments), 0, 0);
     parserUserInput(argv, argc);
 
-    if (argc > 3)
-    {
-        printf("The program needs at most two arguments, exiting...\n");
-        return -1;
-    }
-    else if (argc == 2)
-    {
-        int testLength = convertInput(argv[1]);
-
-        if (testLength < 0)
-        {
-            printf("The correct format is: <prog> <filename> <name_of_test> or <prog> <number> or <prog> <dir_name>\n");
-            return -1;
-        }
-
-        setAttributes(testLength, IRRELEVANT, NOT_APPLICABLE, NOT_APPLICABLE);
-    }
-    else if (argc == 1)
-    {
-        setAttributes(DEFAULT_TEST_LENGTH, IRRELEVANT, NOT_APPLICABLE, NOT_APPLICABLE);
-    }
+    if (getArgValue("-w") == NOT_NULL_ARG)
+        setAttributes(DEFAULT_TEST_LENGTH, argv[2], NOT_APPLICABLE, IGNORE_WHITE_SPACE);
     else
-    {
-        char *buffer =  0; //fileToBuffer(argv[1]);
-//        if ( NULL == buffer)
-//        {
-//            printf("Can't open the files %s\r\nexiting...\n", argv[1]);
-//            return -1;
-//        }
+        setAttributes(DEFAULT_TEST_LENGTH, argv[2], NOT_APPLICABLE, DONT_IGNORE_WHITE_SPACE);
 
-        /* If the test_name starts with an underscore ignore whitespaces. */
-        if (argv[2][0] == '_')
-            setAttributes(DEFAULT_TEST_LENGTH, argv[2], buffer, IGNORE_WHITE_SPACE);
-        else
-            setAttributes(DEFAULT_TEST_LENGTH, argv[2], buffer, DONT_IGNORE_WHITE_SPACE);
-    }
+    int testLength = convertInput(getArgValue("-t"));
+    setAttributes(testLength, IRRELEVANT, NOT_APPLICABLE, NOT_APPLICABLE);
 
     init_sqlite_db();
 
