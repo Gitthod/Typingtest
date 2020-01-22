e59908b9508be96bb8b14ae4853ea488864c8852
Theodore Karatapanis <Theodoros.Karatapanis@rt-rk.com>
2020-01-22 15:40:56 +0100

    Registerd a variable to define the test directory

--- src/main.c
+++ src/main.c
@@ -5,6 +5,7 @@
 #include <speed_test_sqlite.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include "speed_browser.h"
 
 #define DEFAULT_TEST_LENGTH     100
 #define IGNORE_WHITE_SPACE      1
@@ -13,9 +14,10 @@
 #define NOT_APPLICABLE          0
 
 #define ARG0    {"--no-white-spaces","-w", "Automatically skip all the white spaces", argBinary}
-#define ARG1    {"--test-length", "-t", "Set the test length for the automatic tests", argVariable, "71"}
+#define ARG1    {"--test-length", "-l", "Set the test length for the automatic tests", argVariable, "71"}
+#define ARG2    {"--test-directory", "-d", "Set the test length for the automatic tests", argVariable, "./tests"}
 
-static argument arguments[] = {ARG0, ARG1};
+static argument arguments[] = {ARG0, ARG1, ARG2};
 
 int main(int argc, char** argv)
 {
@@ -29,8 +31,9 @@ int main(int argc, char** argv)
     else
         setAttributes(DEFAULT_TEST_LENGTH, argv[2], NOT_APPLICABLE, DONT_IGNORE_WHITE_SPACE);
 
-    int testLength = convertInput(getArgValue("-t"));
+    int testLength = convertInput(getArgValue("-l"));
     setAttributes(testLength, IRRELEVANT, NOT_APPLICABLE, NOT_APPLICABLE);
+    changeTestDir(getArgValue("-d"));
 
     init_sqlite_db();
 
